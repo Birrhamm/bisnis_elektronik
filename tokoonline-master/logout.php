0445bc29e631635476d25cb1d2ce35ece5fbab98
Birrhamefendilubis <115520530+Birrhamm@users.noreply.github.com>
2024-03-16 22:58:45 +0700

    Delete tokoonline-master directory

--- tokoonline-master/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-// Menghancurkan session pelanggan
-session_destroy();
-
-echo "<script>alert('Anda telah logout!');</script>";
-echo "<script>location='index.php';</script>";
